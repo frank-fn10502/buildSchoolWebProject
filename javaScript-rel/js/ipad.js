0807c919323ad3f2d9c0322e5189b5f6a231538b
Frank_Chen <frank08.chen@gmail.com>
2020-04-08 20:47:14 +0800

    apple-ipad-table finish

---
+++ javaScript-rel/js/ipad.js
@@ -0,0 +1,95 @@
+
+export const ipads = [
+    {
+        "sku": "MW762",
+        "price": 10900,
+        "storage": "32GB",
+        "description": "10.2 吋 iPad Wi-Fi 機型 32GB - 金色",
+        "picture": "ipad_gold.png"
+    },
+    {
+        "sku": "MW792",
+        "price": 13900,
+        "storage": "128GB",
+        "description": "10.2 吋 iPad Wi-Fi 機型 128GB - 金色",
+        "picture": "ipad_gold.png"
+    },
+    {
+        "sku": "MW742",
+        "price": 10900,
+        "storage": "32GB",
+        "description": "10.2 吋 iPad Wi-Fi 機型 32GB - 太空灰色",
+        "picture": "ipad_space.png"
+    },
+    {
+        "sku": "MW772",
+        "price": 13900,
+        "storage": "128GB",
+        "description": "10.2 吋 iPad Wi-Fi 機型 128GB - 太空灰色",
+        "picture": "ipad_space.png"
+    },
+    {
+        "sku": "MW752",
+        "price": 10900,
+        "storage": "32GB",
+        "description": "10.2 吋 iPad Wi-Fi 機型 32GB - 銀色",
+        "picture": "ipad_silver.png"
+    },
+    {
+        "sku": "MW782",
+        "price": 13900,
+        "storage": "128GB",
+        "description": "10.2 吋 iPad Wi-Fi 機型 128GB - 銀色",
+        "picture": "ipad_silver.png"
+    },
+    {
+        "sku": "MW6D2",
+        "price": 15400,
+        "storage": "32GB",
+        "description": "10.2 吋 iPad Wi-Fi + 行動網路機型 32GB - 金色",
+        "picture": "ipad_gold.png"
+    },
+    {
+        "sku": "MW6G2",
+        "price": 18400,
+        "storage": "128GB",
+        "description": "10.2 吋 iPad Wi-Fi + 行動網路機型 128GB - 金色",
+        "picture": "ipad_gold.png"
+    },
+    {
+        "sku": "MW6A2",
+        "price": 15400,
+        "storage": "32GB",
+        "description": "10.2 吋 iPad Wi-Fi + 行動網路機型 32GB - 太空灰色",
+        "picture": "ipad_space.png"
+    },
+    {
+        "sku": "MW6E2",
+        "price": 18400,
+        "storage": "128GB",
+        "description": "10.2 吋 iPad Wi-Fi + 行動網路機型 128GB - 太空灰色",
+        "picture": "ipad_space.png"
+    },
+    {
+        "sku": "MW6C2",
+        "price": 15400,
+        "storage": "32GB",
+        "description": "10.2 吋 iPad Wi-Fi + 行動網路機型 32GB - 銀色",
+        "picture": "ipad_silver.png"
+    },
+    {
+        "sku": "MW6F2",
+        "price": 18400,
+        "storage": "128GB",
+        "description": "10.2 吋 iPad Wi-Fi + 行動網路機型 128GB - 銀色",
+        "picture": "ipad_silver.png"
+    }
+];
+
+export const employees = [
+    { name: "Kevin", age: 32, height: 175, weight: 64 },
+    { name: "David", age: 35, height: 170, weight: 60 },
+    { name: "Mary", age: 22, height: 172, weight: 52 },
+    { name: "John", age: 18, height: 178, weight: 70 },
+    { name: "Cindy", age: 26, height: 168, weight: 50 }
+];
